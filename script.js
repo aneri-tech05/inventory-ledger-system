ca17a3e4291d22787f5870d271bed1d720352432
Shreya Trivedi <shreyatrivedi.23.cse@iite.indusuni.ac.in>
2026-03-14 14:38:16 +0530

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const animation = document.querySelector(".bg-animation");
 
+if(animation){
+
 for(let i=0;i<50;i++){
 
 let span = document.createElement("span");
@@ -9,10 +11,12 @@ let size = Math.random()*20;
 span.style.width = 10 + size + "px";
 span.style.height = 10 + size + "px";
 
-span.style.left = Math.random()*window.innerWidth + "px";
+span.style.left = Math.random()*100 + "%";
 
 span.style.animationDuration = 10 + Math.random()*20 + "s";
 
 animation.appendChild(span);
+
+}
 
 }
